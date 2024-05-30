8b89d87d9ad21a4c93b91cb1bf0c6aacd4f5e8af
AndSim <siminiucandrew@gmail.com>
2024-05-30 23:03:43 +0300

    feat: optimize quality of Image with Next.js

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,9 +1,17 @@
 import Link from 'next/link.js';
+import Image from 'next/image';
+import logo from '@/public/logo.png';
 
 function Logo() {
 	return (
 		<Link href="/" className="z-10 flex items-center gap-4">
-			<img src="/logo.png" height="60" width="60" alt="The Wild Oasis logo" />
+			<Image
+				src={logo}
+				height="60"
+				width="60"
+				quality={100}
+				alt="The Wild Oasis logo"
+			/>
 			<span className="text-primary-100 text-xl font-semibold">
 				The Wild Oasis
 			</span>
