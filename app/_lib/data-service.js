13e2eb972659ffdaca9af3aedda6c20c7548316f
AndSim <siminiucandrew@gmail.com>
2024-06-01 21:10:53 +0300

    feat: add nested routes and nested pages (app/profile/page.js)guest profile, (app/reservations/page.js)user reservations

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,8 +1,5 @@
 import { eachDayOfInterval } from 'date-fns';
 
-/////////////
-// GET
-
 export async function getCabin(id) {
 	const { data, error } = await supabase
 		.from('cabins')
@@ -10,7 +7,6 @@ export async function getCabin(id) {
 		.eq('id', id)
 		.single();
 
-	// For testing
 	// await new Promise((res) => setTimeout(res, 1000));
 
 	if (error) {
@@ -48,7 +44,6 @@ export const getCabins = async function () {
 	return data;
 };
 
-// Guests are uniquely identified by their email address
 export async function getGuest(email) {
 	const { data, error } = await supabase
 		.from('guests')
@@ -56,7 +51,6 @@ export async function getGuest(email) {
 		.eq('email', email)
 		.single();
 
-	// No error here! We handle the possibility of no guest in the sign in callback
 	return data;
 }
 
@@ -78,7 +72,6 @@ export async function getBooking(id) {
 export async function getBookings(guestId) {
 	const { data, error, count } = await supabase
 		.from('bookings')
-		// We actually also need data on the cabins as well. But let's ONLY take the data that we actually need, in order to reduce downloaded data.
 		.select(
 			'id, created_at, startDate, endDate, numNights, numGuests, totalPrice, guestId, cabinId, cabins(name, image)',
 		)
@@ -98,7 +91,6 @@ export async function getBookedDatesByCabinId(cabinId) {
 	today.setUTCHours(0, 0, 0, 0);
 	today = today.toISOString();
 
-	// Getting all bookings
 	const { data, error } = await supabase
 		.from('bookings')
 		.select('*')
@@ -110,7 +102,6 @@ export async function getBookedDatesByCabinId(cabinId) {
 		throw new Error('Bookings could not get loaded');
 	}
 
-	// Converting to actual dates to be displayed in the date picker
 	const bookedDates = data
 		.map((booking) => {
 			return eachDayOfInterval({
@@ -146,9 +137,6 @@ export async function getCountries() {
 	}
 }
 
-/////////////
-// CREATE
-
 export async function createGuest(newGuest) {
 	const { data, error } = await supabase.from('guests').insert([newGuest]);
 
@@ -164,7 +152,6 @@ export async function createBooking(newBooking) {
 	const { data, error } = await supabase
 		.from('bookings')
 		.insert([newBooking])
-		// So that the newly created object gets returned!
 		.select()
 		.single();
 
@@ -176,10 +163,6 @@ export async function createBooking(newBooking) {
 	return data;
 }
 
-/////////////
-// UPDATE
-
-// The updatedFields is an object which should ONLY contain the updated data
 export async function updateGuest(id, updatedFields) {
 	const { data, error } = await supabase
 		.from('guests')
@@ -209,9 +192,6 @@ export async function updateBooking(id, updatedFields) {
 	}
 	return data;
 }
-
-/////////////
-// DELETE
 
 export async function deleteBooking(id) {
 	const { data, error } = await supabase.from('bookings').delete().eq('id', id);
--- app/account/page.js
+++ app/account/page.js
@@ -4,8 +4,8 @@ export const metadata = {
 
 export default function Page() {
 	return (
-		<div>
-			<h2>Your Account</h2>
-		</div>
+		<h2 className="font-semibold text-2xl text-accent-400 mb-7">
+			Welcome, User
+		</h2>
 	);
 }
