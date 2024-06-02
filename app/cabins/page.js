ebbebd3ba6834ab38b46819133872d56125bdb0c
AndSim <siminiucandrew@gmail.com>
2024-06-02 23:32:15 +0300

    feat: (cabins/page.js) render cabin list(CabinCard) data from supabase

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,11 +1,12 @@
 import CabinCard from '@/app/_components/CabinCard';
+import { getCabins } from '@/app/_lib/data-service';
 
 export const metadata = {
 	title: 'Cabins',
 };
 
-export default function Page() {
-	const cabins = [];
+export default async function Page() {
+	const cabins = await getCabins();
 
 	return (
 		<div>
