f4cda14e07dfdf11b783fb1cabc80b4c2c76b96f
AndSim <siminiucandrew@gmail.com>
2024-06-06 09:56:12 +0300

    feat: add global error handling (error.js)(not-found.js)

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -1,5 +1,6 @@
 import { eachDayOfInterval } from 'date-fns';
 import { supabase } from '@/app/_lib/supabase';
+import { notFound } from 'next/navigation';
 
 export async function getCabin(id) {
 	const { data, error } = await supabase
@@ -9,7 +10,7 @@ export async function getCabin(id) {
 		.single();
 
 	if (error) {
-		console.error(error);
+		notFound();
 	}
 
 	return data;
