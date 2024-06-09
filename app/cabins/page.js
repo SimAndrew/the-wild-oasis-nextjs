976a631055720d2d490836aba09c65bd17cf731d
AndSim <siminiucandrew@gmail.com>
2024-06-09 23:43:02 +0300

    feat: In (cabins/page.js)(/cabins) build a filter component (Filter.js)

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,16 @@
 import { Suspense } from 'react';
 import CabinList from '@/app/_components/CabinList';
 import Spinner from '@/app/_components/Spinner';
+import Filter from '@/app/_components/Filter';
 
 export const revalidate = 3600;
-
 export const metadata = {
 	title: 'Cabins',
 };
 
-export default function Page() {
+export default function Page({ searchParams }) {
+	const filter = searchParams?.capacity ?? 'all';
+
 	return (
 		<div>
 			<h1 className="text-4xl mb-5 text-accent-400 font-medium">
@@ -23,8 +25,12 @@ export default function Page() {
 				Welcome to paradise.
 			</p>
 
-			<Suspense fallback={<Spinner />}>
-				<CabinList />
+			<div className="flex justify-end mb-8">
+				<Filter />
+			</div>
+
+			<Suspense fallback={<Spinner />} key={filter}>
+				<CabinList filter={filter} />
 			</Suspense>
 		</div>
 	);
