2cbcc4274fd405d1d576bf50c2b9d9ed3d7480fb
AndSim <siminiucandrew@gmail.com>
2024-06-14 17:45:09 +0300

    feat: build a signOut button (SignOutButton.js) (/account)route

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,7 +1,11 @@
 'use server';
 
-import { signIn } from '@/app/_lib/auth';
+import { signIn, signOut } from '@/app/_lib/auth';
 
 export async function signInAction() {
 	await signIn('google', { redirectTo: '/account' });
+}
+
+export async function signOutAction() {
+	await signOut({ redirectTo: '/' });
 }
