a6c902e06cf98d09cac6de61cc82531902a15f00
AndSim <siminiucandrew@gmail.com>
2024-06-14 14:49:01 +0300

    feat: add avatar of logged in user(Navigation.js). Only logged user able reserve cabin (Reservation.js) Authentication

--- app/_components/Reservation.js
+++ app/_components/Reservation.js
@@ -1,12 +1,16 @@
 import DateSelector from '@/app/_components/DateSelector';
 import ReservationForm from '@/app/_components/ReservationForm';
 import { getBookedDatesByCabinId, getSettings } from '@/app/_lib/data-service';
+import { auth } from '@/app/_lib/auth';
+import LoginMessage from '@/app/_components/LoginMessage';
 
 async function Reservation({ cabin }) {
 	const [settings, bookedDates] = await Promise.all([
 		getSettings(),
 		getBookedDatesByCabinId(cabin.id),
 	]);
+
+	const session = await auth();
 
 	return (
 		<div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
@@ -15,7 +19,11 @@ async function Reservation({ cabin }) {
 				bookedDates={bookedDates}
 				cabin={cabin}
 			/>
-			<ReservationForm cabin={cabin} />
+			{session?.user ? (
+				<ReservationForm cabin={cabin} user={session.user} />
+			) : (
+				<LoginMessage />
+			)}
 		</div>
 	);
 }
--- app/cabins/loading.js
+++ app/cabins/loading.js
@@ -3,7 +3,7 @@ import Spinner from '@/app/_components/Spinner';
 export default function Loading() {
 	return (
 		<div className="grid items-center justify-center">
-			<Spinner />;
+			<Spinner />
 			<p className="text-xl text-primary-200">Loading Cabin data...</p>
 		</div>
 	);
