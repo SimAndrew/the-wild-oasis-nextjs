a502eadf8f046f4b4a29a081381a15848c33a7cd
AndSim <siminiucandrew@gmail.com>
2024-06-18 15:17:33 +0300

    feat: in reservation page guest can Delete their upcoming reservations (_components/DeleteReservation.js) (/account/reservations)route

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { auth, signIn, signOut } from '@/app/_lib/auth';
 import { supabase } from '@/app/_lib/supabase';
 import { revalidatePath } from 'next/cache';
+import { getBookings } from '@/app/_lib/data-service';
 
 export async function updateGuest(formData) {
 	const session = await auth();
@@ -28,6 +29,25 @@ export async function updateGuest(formData) {
 	if (error) throw new Error('Guest could not be updated!');
 
 	revalidatePath('/account/profile');
+}
+
+export async function deleteReservation(bookingId) {
+	const session = await auth();
+	if (!session) throw new Error('You must be Logged In!');
+
+	const guestBookings = await getBookings(session.user.guestId);
+	const guestBookingIds = guestBookings.map((booking) => booking.id);
+	if (!guestBookingIds.includes(bookingId))
+		throw new Error('You are not allowed to delete this booking!');
+
+	const { error } = await supabase
+		.from('bookings')
+		.delete()
+		.eq('id', bookingId);
+
+	if (error) throw new Error('Booking could not be deleted!');
+
+	revalidatePath('/account/reservations');
 }
 
 export async function signInAction() {
