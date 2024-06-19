86c092aa78d844ac0672d68a89587f9ed4c4d17a
AndSim <siminiucandrew@gmail.com>
2024-06-19 14:17:50 +0300

    feat: add func date selector in reservation cabin page

--- app/_components/DateSelector.js
+++ app/_components/DateSelector.js
@@ -1,9 +1,14 @@
 'use client';
 
-import { isWithinInterval } from 'date-fns';
+import {
+	differenceInDays,
+	isSameDay,
+	isWithinInterval,
+	isPast,
+} from 'date-fns';
 import { DayPicker } from 'react-day-picker';
-import 'react-day-picker/dist/style.css';
 import { useReservation } from '@/app/_components/ReservationContext';
+import 'react-day-picker/dist/style.css';
 
 function isAlreadyBooked(range, datesArr) {
 	return (
@@ -18,10 +23,11 @@ function isAlreadyBooked(range, datesArr) {
 function DateSelector({ settings, cabin, bookedDates }) {
 	const { range, setRange, resetRange } = useReservation();
 
-	const regularPrice = 23;
-	const discount = 23;
-	const numNights = 23;
-	const cabinPrice = 23;
+	const displayRange = isAlreadyBooked(range, bookedDates) ? {} : range;
+
+	const { regularPrice, discount } = cabin;
+	const numNights = differenceInDays(displayRange.to, displayRange.from);
+	const cabinPrice = numNights * (regularPrice - discount);
 
 	const { minBookingLength, maxBookingLength } = settings;
 
@@ -30,9 +36,8 @@ function DateSelector({ settings, cabin, bookedDates }) {
 			<DayPicker
 				className="pt-12 place-self-center"
 				mode="range"
-				// onSelect={setRange}
 				onSelect={(selectedRange) => selectedRange && setRange(selectedRange)}
-				selected={range}
+				selected={displayRange}
 				min={minBookingLength + 1}
 				max={maxBookingLength}
 				fromMonth={new Date()}
@@ -40,6 +45,10 @@ function DateSelector({ settings, cabin, bookedDates }) {
 				toYear={new Date().getFullYear() + 5}
 				captionLayout="dropdown"
 				numberOfMonths={2}
+				disabled={(curDate) =>
+					isPast(curDate) ||
+					bookedDates.some((date) => isSameDay(date, curDate))
+				}
 			/>
 
 			<div className="flex items-center justify-between px-8 bg-accent-500 text-primary-800 h-[72px]">
